594987244b9970ffdb678fe20cd006a328ed8516
czarra <radomich11@gmail.com>
2017-11-05 18:57:59 +0100

    add icon and change name

--- feature/src/main/java/com/example/rad/test/feature/MainActivity.java
+++ feature/src/main/java/com/example/rad/test/feature/MainActivity.java
@@ -43,7 +43,11 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.category) {
+            return true;
+        }
+        if (id == R.id.brands) {
+
             return true;
         }
 
